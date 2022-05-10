5ffd323002303f85bf250229b65f9e754f8ef1c3
user <negiiinn.nm@gmail.com>
2022-05-10 22:56:17 +0430

    Initial commit

---
+++ src/main/java/ir/ac/kntu/RecursiveGUI.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu;
+
+import com.sun.javafx.scene.paint.GradientUtils;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Rotate;
+
+import java.util.Random;
+
+public class RecursiveGUI {
+
+}
